f362a40fcec19cd9d97749c786bbc7d245ea251e
Alisson-Alex <alisson.xfx@gmail.com>
2025-04-03 13:08:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,8 +40,8 @@ function showTasks(){ //Função mostrar cada tarefa da lista
                     <h3>${item.title}</h3>
                     <p>${item.description}</p>
                 </div>
-                <img url="https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/confirm.png" alt="check-na-tarefa" onclick="completeTask(${position1})">
-                <img url="https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/delete.png" alt="tarefa-para-lixo" onclick="deleteItem(${position1})">
+                <img url("https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/confirm.png") alt="check-na-tarefa" onclick="completeTask(${position1})">
+                <img url("https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/delete.png") alt="tarefa-para-lixo" onclick="deleteItem(${position1})">
             </li>
 
         `
