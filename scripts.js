99b5b0bc8ffa85adbe43709f1cc757586be233f1
Alisson-Alex <alisson.xfx@gmail.com>
2025-04-03 12:56:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,18 +32,19 @@ function showTasks(){ //Função mostrar cada tarefa da lista
     let newLi = ''
 
     myList.forEach((item, position1) => { //Lê item por item no array
-        newLi = newLi + `
+        newLi = newLi + 
+        `
 
             <li class="task ${item.complete && "done"}">   
                 <div>
                     <h3>${item.title}</h3>
                     <p>${item.description}</p>
                 </div>
-                    <img src="https://github.com/Alisson-Alex/Aplica-o-To-Do-List/blob/main/img/confirm.png" alt="check-na-tarefa" onclick="completeTask(${position1})">
-                    <img src="https://github.com/Alisson-Alex/Aplica-o-To-Do-List/blob/main/img/delete.png" alt="tarefa-para-lixo" onclick="deleteItem(${position1})">
+                <img src="https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/confirm.png" alt="check-na-tarefa" onclick="completeTask(${position1})">
+                <img src="https://raw.githubusercontent.com/Alisson-Alex/Aplica-o-To-Do-List/main/img/delete.png" alt="tarefa-para-lixo" onclick="deleteItem(${position1})">
             </li>
 
-            `
+        `
     }) 
 
     listComplete.innerHTML = newLi
